83348e8356adb4cdaa5bc1f895e40c1bca61361c
Nikita Mingazov <nikita.mingazov@dz.ru>
2014-05-06 13:34:30 +0400

    some test added, resolve #1

--- src/test/java/ru/cll/search/mvcDispatcherTests.java
+++ src/test/java/ru/cll/search/mvcDispatcherTests.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -11,8 +12,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -31,7 +31,22 @@ public class mvcDispatcherTests {
     }
 
     @Test
-    public void simple() throws Exception {
-        assert(true);
+    public void aboutInitialText() throws Exception {
+        mockMvc.perform(
+                    get("/getDirtyAnnotations")
+                        .param("text", "HaddaWay")
+                )
+                .andExpect(status().isOk())
+                .andExpect(request().attribute("initialText", "HaddaWay"));
+    }
+
+    @Test
+    public void aboutXmlView() throws Exception {
+        mockMvc.perform(
+                get("/getXmi")
+                        .param("text", "Немного русского языка")
+        )
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(new MediaType("application", "xml")));
     }
 }
